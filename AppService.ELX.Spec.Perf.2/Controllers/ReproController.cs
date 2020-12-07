24aa473a23d74836e64f09453071c9c7f07862bd
Juntao Zhu <juzhu@microsoft.com>
2020-12-07 13:24:15 +0800

    Add project files.

---
+++ AppService.ELX.Spec.Perf.2/Controllers/ReproController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AppService.ELX.Spec.Perf._2.Controllers
+{
+    public class ReproController : Controller
+    {
+        static Lazy<HttpClient> client = new Lazy<HttpClient>();
+        public async Task<string> Index()
+        {
+            HttpClient c = client.Value;
+            HttpResponseMessage response = await c.GetAsync("http://juzhu500.azurewebsites.net/");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return "Done";
+        }
+    }
+}
